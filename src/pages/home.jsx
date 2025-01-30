5c1789d459e738f3caf441f9c9be603020b28b6e
nicollasprado <nicollasprado.profissional@gmail.com>
2025-01-30 13:59:47 -0300

    adição dos links na main page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,34 +1,57 @@
+import { FileText, Github, Linkedin, Mail } from "lucide-react";
+
 function Home( { scrollToSection } ){
     return (
-        <div className="flex flex-col justify-center items-center h-screen leading-30">
-            <div>
-                <h2 className="text-amber-400 font-extrabold text-6xl ml-1.5">Olá, sou Nicollas</h2>
-                <h1 className="text-amber-300 font-black">BACKEND</h1>
-                <h1 className="text-amber-300 font-black">DEVELOPER</h1>
-                
-                <ol className="flex justify-around">
-                    <li>
-                        <button
-                        className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
-                        onClick={() => scrollToSection('sobremim')}
-                        >Sobre mim</button>
-                    </li>
-                    <li>
-                        <button
-                        className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
-                        onClick={() => scrollToSection('projetos')}
-                        >Projetos</button>
-                    </li>
-                    <li>
-                        <button
-                        className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
-                        onClick={() => scrollToSection('cursos')}
-                        >Cursos</button>
-                    </li>
-                    <li>
-                        <button className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold">Links</button>
-                    </li>
-                </ol>
+        <div className="flex items-center justify-center">
+            <ol className="flex flex-col gap-5 pb-10">
+                <li>
+                    <a href="https://github.com/nicollasprado" target="_blank">
+                        <Github className="text-green-400 hover:fill-blue-400 hover:text-blue-300 size-11" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.linkedin.com/in/nicollas-prado-420082302/" target="_blank">
+                        <Linkedin className="text-green-400 hover:fill-blue-400 hover:text-blue-300 size-11" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://drive.google.com/drive/folders/1NoIi5SDxFha1tm8lglFRkdtgHW4qIx7T?usp=sharing" target="_blank">
+                        <FileText className="text-green-400 hover:fill-blue-400 hover:text-blue-300 size-11" />
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:nicollasprado.profissional@gmail.com" target="_blank">
+                        <Mail className="text-green-400 hover:fill-blue-400 hover:text-blue-300 size-11"/>
+                    </a>
+                </li>
+            </ol>
+            <div className="flex items-center h-screen leading-30">
+                <div>
+                    <h2 className="text-amber-400 font-extrabold text-6xl ml-1.5">Olá, sou Nicollas</h2>
+                    <h1 className="text-amber-300 font-black">BACKEND</h1>
+                    <h1 className="text-amber-300 font-black">DEVELOPER</h1>
+                    
+                    <ol className="flex justify-around">
+                        <li>
+                            <button
+                            className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
+                            onClick={() => scrollToSection('sobremim')}
+                            >Sobre mim</button>
+                        </li>
+                        <li>
+                            <button
+                            className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
+                            onClick={() => scrollToSection('projetos')}
+                            >Projetos</button>
+                        </li>
+                        <li>
+                            <button
+                            className="hover:cursor-pointer hover:text-emerald-500 duration-500 text-amber-400 text-2xl font-bold"
+                            onClick={() => scrollToSection('cursos')}
+                            >Cursos</button>
+                        </li>
+                    </ol>
+                </div>
             </div>
         </div>
     );
--- src/pages/sobre.jsx
+++ src/pages/sobre.jsx
@@ -9,7 +9,7 @@ function Sobre(){
                 <div className="flex flex-col gap-10 bg-purple-700 w-300 h-160 rounded-r-3xl p-5">
                     <div className="flex flex-col gap-5">
                         <p className="text-white font-normal text-2xl">Olá, meu nome é <b className="text-green-400">Nicollas Prado</b> e já estou estudando desenvolvimento de software a 2 anos. Atualmente estou no 2º período do curso de <b className="text-green-400">Análise e Desenvolvimento de Software no</b> <a href="https://portal.ifrn.edu.br/" target="_blank"><b className="text-emerald-400 underline hover:text-green-300">IFRN</b></a> onde já estudei matérias fundamentais para a área como Algoritmos, Programação Orientada a Objetos, Web Design e Arquitetura de Computadores.</p>
-                        <p className="text-white font-normal text-2xl">Além da faculdade, eu <b className="text-green-400">estou sempre em busca de novos conhecimentos e aplicando-os em projetos pessoais</b> que ajudem a desenvolver minhas habilidades técnicas e pessoais. Meu principal projeto pessoal atual é um site de E-commerce com o backend feito em Java com Springboot e o frontend em Reactjs e tailwindcss.</p>
+                        <p className="text-white font-normal text-2xl">Além da faculdade, eu <b className="text-green-400">estou sempre em busca de novos conhecimentos e aplicando-os em projetos pessoais</b> que ajudem a desenvolver minhas habilidades técnicas e pessoais. Ademais, tenho um nível de <b className="text-green-400">inglês avançado</b> conseguindo falar, entender, ler e escrever nesse idioma sem dificuldades.</p>
                     </div>
 
                     <h2 className="text-amber-400 font-bold text-3xl">Minhas Principais Tecnologias:</h2>
